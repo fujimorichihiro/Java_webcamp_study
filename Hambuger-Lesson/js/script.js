eca88b7b9b5df6e69f98ffdebe5023b6342b7a41
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-25 21:33:42 +0900

    [Finish]6-10

--- Hambuger-Lesson/js/script.js
+++ Hambuger-Lesson/js/script.js
@@ -1,8 +1,8 @@
 $(function() {
  
   $('.menu-trigger').on('click', function() {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
+    $(this).toggleClass('active');//class属性の追加と削除
+    $('#sp-menu').fadeToggle();//fadeinとfadeoutを交互に行う
     return false;
   });
  
