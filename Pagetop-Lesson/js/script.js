b4b12334a00cfe58365400a8df533fc6fabceada
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-25 21:01:37 +0900

    [Start]6-10

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -3,7 +3,7 @@ $(function() {
   $('#back a').on('click',function(){ //id(back)内のaタグに
     $('body, html').animate({ //bodyもしくはhtml
       scrollTop:0 //スクロール位置を指定、0だと最上部
-    }, 800);
+    }, 800); //変化時間の指定
       return false;
   });
  
