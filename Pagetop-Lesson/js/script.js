184aea03e96f069a61e938313f3c6dbaae8f36bf
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-25 20:32:13 +0900

    [Finish]6-9

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -1,8 +1,8 @@
 $(function() {
  
-  $('#back a').on('click',function(){
-    $('body, html').animate({
-      scrollTop:0
+  $('#back a').on('click',function(){ //id(back)内のaタグに
+    $('body, html').animate({ //bodyもしくはhtml
+      scrollTop:0 //スクロール位置を指定、0だと最上部
     }, 800);
       return false;
   });
