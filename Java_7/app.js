15899f5391379134bdfb2857bd7418965c2a24c4
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-25 15:03:37 +0900

    [Finish]6-7

--- Java_7/app.js
+++ Java_7/app.js
@@ -1,5 +1,5 @@
 $(function(){
     $('button').on('click', function(){  //botton要素がクリックされたとき処理を実行
-        $('ul').children().css('color','red'); //ul要素の子要素
+        $('ul').children().css('color','red'); //ul要素の子要素が対象となる。
     });
 });// box1の中でクリックされた要素がスライドする。
