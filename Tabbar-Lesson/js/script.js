175f4d4112b6d90454ce18efeb8347fa30544b50
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-25 22:12:52 +0900

    [Finish]6-11

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,9 +1,9 @@
 $('#tab-contents .tab[id != "tab1"]').hide();
  
 $('#tab-menu a').on('click', function() {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
+  $("#tab-contents .tab").hide(); //隠す
+  $("#tab-menu .active").removeClass("active"); //activeclaassを取り消す
+  $(this).addClass("active"); //cliskされたthisにactiveclassと追加する
+  $($(this).attr("href")).show(); //
   return false;
 });
